9756967ae91fad4c7524137b2a57d41af58c6b1d
himanshu <rathihimanshu@live.com>
2026-01-27 17:43:10 +0530

    added resources

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
 # resource "aws_instance" "ec2" {
 #     ami           = "ami-0c55b159cbfafe1f0"
 #     instance_type = "t2.micro"
-  
+
 # }
 resource "aws_s3_bucket" "bucket1" {
-    bucket = "himansh-testbucket-27jan2026"
+  bucket = "himansh-testbucket-27jan2026"
+
+}
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "himansh-testbucket-27jan2026-new"
+
+}
+
+resource "aws_vpc" "vpc1" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "somevpc"
+    }
+}
+
+resource "aws_instance" "ec2-vm1" {
+    ami           = "ami-055a9df0c8c9f681c"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "first-vm"
+    }
   
 }
--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 
 }
 # provider "google" {
-  
+
 # }
 # provider "kubernetes" {
-  
+
 # }
 # provider "azurerm" {
-  
+
 # }
